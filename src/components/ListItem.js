ae16bfb94d58f485a6bfd4c2432df1d05c699fad
Barret J. Nobel <bear.nobel@gmail.com>
2017-12-09 10:59:35 -0500

    added mapStateToProps to get the selectedLibID from the reducer - wrote function to be called whenever a list item is clicked which will display the text from the json object

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,18 +4,32 @@ import * as actions from "../actions";
 import { connect } from "react-redux";
 
 class ListItem extends Component {
+  renderDescription() {
+    const { id, description } = this.props.library.item;
+
+    if( id === this.props.selectedLibraryID ){
+      return(
+        <Text>{description}</Text>
+      );
+    }
+  }
+
   render() {
-    console.log(this.props);
     const { id, title } = this.props.library.item;
 
     return (
       <TouchableWithoutFeedback onPress={ () => this.props.selectLibrary(id)}>
         <View>
           <Text>{title}</Text>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
   }
 }
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = state => {
+  return { selectedLibraryID: state.selectedLibraryID };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
